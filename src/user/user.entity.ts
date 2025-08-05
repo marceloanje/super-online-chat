a4d1167e0acb65332e7935c0b3d9c163e9c7e807
marceloanje <marcelo.anje@outlook.com>
2025-08-05 19:30:39 -0300

    feat: add user module

---
+++ src/user/user.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 20 })
+  phoneNumber: string;
+}
