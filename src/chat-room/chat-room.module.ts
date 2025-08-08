e2a3256f1660fa58e658498113a8f8e2f8a09a82
marceloanje <marcelo.anje@outlook.com>
2025-08-08 20:31:42 -0300

    feat: add definition of repository for entities

--- src/chat-room/chat-room.module.ts
+++ src/chat-room/chat-room.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatRoom } from './chat-room.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ChatRoom])],
+})
 export class ChatRoomModule {}
--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from './messages.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Message])],
+})
 export class MessagesModule {}
