0a6ceabcd62b53634b52bf8276d398db02cb22f0
marceloanje <marcelo.anje@outlook.com>
2025-08-08 10:00:56 -0300

    feat: sets up the initial connection to the databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ChatRoomModule } from './chat-room/chat-room.module';
+import { MessagesModule } from './messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { ChatRoom } from './chat-room/chat-room.entity';
+import { Message } from './messages/messages.entity';
 
 @Module({
-  imports: [UserModule, ChatRoomModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'super_chat_online',
+      entities: [User, ChatRoom, Message],
+      synchronize: true,
+    }),
+    UserModule,
+    ChatRoomModule,
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
