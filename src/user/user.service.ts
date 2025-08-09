ac38855c56d9a6f4f8bca3b1cdac0f24018a62e1
marceloanje <marcelo.anje@outlook.com>
2025-08-09 00:56:56 -0300

    feat: add service for User module

---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(dto: CreateUserDto) {
+    const user = this.userRepository.create(dto);
+
+    return user;
+  }
+}
