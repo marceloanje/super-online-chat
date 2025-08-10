ec5a2089b627870d90a8147a978b64a203423b77
marceloanje <marcelo.anje@outlook.com>
2025-08-09 22:06:44 -0300

    feat: update ChatRoom module for export providers and controllers

--- src/chat-room/chat-room.module.ts
+++ src/chat-room/chat-room.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatRoom } from './chat-room.entity';
+import { ChatRoomsService } from './chat-room.service';
+import { ChatRoomsController } from './chat-room.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatRoom])],
+  providers: [ChatRoomsService],
+  controllers: [ChatRoomsController],
 })
 export class ChatRoomModule {}
