5c96d1e30a80059a70a5e506cc0b9e609485e734
marceloanje <marcelo.anje@outlook.com>
2025-08-09 22:08:12 -0300

    feat: add sync method for create

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,7 +15,7 @@ export class UsersController {
   }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): User {
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 }
